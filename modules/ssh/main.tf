5eb37ae78893ac0ddf4b19b8f22fa7bcb174b4a6
Tania Duggal <103496926+taniaduggal@users.noreply.github.com>
2024-01-30 09:34:50 +0530

    Update main.tf

--- modules/ssh/main.tf
+++ modules/ssh/main.tf
@@ -7,7 +7,7 @@ resource "azurerm_ssh_public_key" "ssh_key_pair" {
   name      = random_pet.ssh_key_name.id
   resource_group_name = var.resource_group_name
   location  = var.resource_group_location
-  public_key          = "${{ secrets.SSH_PUBLIC_KEY }}"
+  public_key = "${{ secrets.SSH_PUBLIC_KEY }}"
 
   tags = var.common_tags
 }
